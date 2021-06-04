abfe8fe3737ea02f9bd9a89ac2bf09c39d0a893b
aless <alessiocorrado99@gmail.com>
2021-06-04 11:42:40 +0200

    ok

--- main.hs
+++ main.hs
@@ -1,12 +1,15 @@
 module Esercizi where
 
+  --1.1
 factorial :: Integer -> Integer
 factorial 0 = 1
 factorial n = n * factorial (n-1)
 
+--1.2
 combinazioni :: Integer -> Integer -> Integer
 combinazioni n k = div (factorial n)  ((factorial k) * (factorial (n-k)))
 
+--1.3
 combinazioniElementi :: (Eq a) => [a] -> [[a]]
 combinazioniElementi [] = [[]]
 combinazioniElementi xs = foldr (\x acc -> x ++ acc) [] (map (\x -> (map (\y -> x:y) (combinazioniElementi [t | t <- xs, t /= x]))) xs)
@@ -20,9 +23,11 @@ rimuoviPosPari = rimuoviPos odd
 rimuoviPosDispari :: [a] -> [a]
 rimuoviPosDispari = rimuoviPos even
 
+--2.2
 sommaPosDispari :: (Num a) => [a] -> a
 sommaPosDispari = sum . rimuoviPosPari
 
+--2.3
 quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
 quickSort (x:xs) = (quickSort $ filter (<=x) xs) ++ [x] ++ (quickSort $ filter (>x) xs)
@@ -35,15 +40,19 @@ dueMinori xs = (Just a, Just b) where (a:b:_) = (quickSort xs)
 dispari :: [Integer] -> [Integer]
 dispari = filter odd
 
+--2.4
 dueMinoriDispari :: [Integer] -> (Maybe Integer, Maybe Integer)
 dueMinoriDispari = dueMinori . dispari
 
+--2.5
 coppieConSommeSuffisse :: Num b => [b] -> [(b, b)]
 coppieConSommeSuffisse xs = zip xs (tail $ scanr(+) 0 xs)
 
+--2.6
 coppieConSommePrefisse :: Num b => [b] -> [(b, b)]
 coppieConSommePrefisse xs = zip xs (scanl(+) 0 xs)
 
+--2.7
 shiftToZero :: (Num a, Ord a) => [a] -> [a]
 shiftToZero xs = res
   where
@@ -56,6 +65,7 @@ shiftToZero xs = res
 allSame :: (Eq a) => [a] -> Bool
 allSame v = all (== (head v)) v
 
+--3.1
 matrix_dim :: [[a]] -> (Int, Int)
 matrix_dim mat
   | c == -1 = (-1, -1)
@@ -82,15 +92,18 @@ prodVec = opElementwise (*)
 opColumnsAccumulation :: ([a] -> [a] -> [a]) -> [[a]] -> [a]
 opColumnsAccumulation op = foldl1(\acc x -> op acc x)
 
+--3.2
 colsums :: Num a => [[a]] -> [a]
 colsums = opColumnsAccumulation sumVec
 
 invertiSegno :: Num a => [a] -> [a]
 invertiSegno = map (\x -> -x)
 
+--3.3
 colaltsums :: Num a => [[a]] -> [a]
 colaltsums v = sumVec (colsums $ rimuoviPosPari v) (colsums $ map invertiSegno $ rimuoviPosDispari v)
 
+--3.4
 colminmax :: Ord b => [[b]] -> [(b, b)]
 colminmax mat = zip (opColumnsAccumulation minVec mat) (opColumnsAccumulation maxVec mat)
 
@@ -103,26 +116,32 @@ filterMatrix pred m = map snd . filter (\((r,c), _) -> pred r c)
 
 allZero pred m = all (==0) $ filterMatrix pred m
 
+--3.5
 lowertriangular :: (Eq a, Num a) => [[a]] -> Bool
 lowertriangular = allZero (>)
 
+--3.6
 uppertriangular :: (Eq a, Num a) => [[a]] -> Bool
 uppertriangular m = allZero (<) m
 
+--3.7
 diagonal :: (Eq a, Num a) => [[a]] -> Bool
 diagonal m = allZero (/=) m
 
+--3.8
 convergent :: (Ord a, Num a) => [[a]] -> a -> Bool
 convergent = convergentAux (0:(cycle [1]))
   where convergentAux :: (Ord a, Num a) => [a] -> [[a]] -> a -> Bool
         convergentAux _ [] _ = True
         convergentAux v (x:xs) r = ((<r) $ abs $ sum $ prodVec x v) && (convergentAux (1:v) xs r)
 
+--3.9
 transpose :: [[a]] -> [[a]]
 transpose [] = []
 transpose (x:[]) = [[t] | t <- x]
 transpose (x:xs) = map (\(l, e) -> e:l) $ zip (transpose xs) x
 
+--3.10
 isSymmetric :: Eq a => [[a]] -> Bool
 isSymmetric m = m == (transpose m)
 
@@ -130,6 +149,7 @@ toMatrix :: Int -> [a] -> [[a]]
 toMatrix _ [] = []
 toMatrix ncols m = (take ncols m):(toMatrix ncols (drop ncols m))
 
+--3.11
 matProduct :: Num a => [[a]] -> [[a]] -> [[a]]
 matProduct a b = toMatrix (length $ head b) $ map (\(a, b) -> sum $ prodVec a b) $ prodottoCartesiano a (transpose b)
 
@@ -138,17 +158,21 @@ data BST a = Void | Node {
   left, right :: BST a
 } deriving (Eq, Ord, Read, Show)
 
+--4.1
 sommaAlbero :: Num p => BST p -> p
 sommaAlbero Void = 0
 sommaAlbero n = val n + (sommaAlbero $ left n) + (sommaAlbero $ right n)
 
+--4.2
 sommaDispariAlbero Void = 0
 sommaDispariAlbero n = (if odd v then v else 0) + (sommaDispariAlbero $ left n) + (sommaDispariAlbero $ right n)
   where v = val n
 
+--4.3
 samesums :: (Eq a, Num a) => [BST a] -> Bool
 samesums v = allSame $ map sommaAlbero $ v
 
+--4.4
 bstelem :: (Ord a, Eq a) => a -> BST a -> Bool
 _ `bstelem` Void = False
 x `bstelem` n
@@ -157,6 +181,7 @@ x `bstelem` n
   | otherwise = x `bstelem` (right n)
   where cur = val n
 
+--4.5
 bstinsert :: (Ord a, Eq a) => a -> BST a -> BST a
 bstinsert x Void = Node {val=x, left=Void, right=Void}
 bstinsert x node
@@ -164,6 +189,7 @@ bstinsert x node
   | otherwise = Node{ val=(val node), left=(left node), right= (bstinsert x (right node))}
   where cur = val node
 
+--4.6
 bstToVec :: BST a -> [a]
 bstToVec Void = []
 bstToVec node = (bstToVec $ left node) ++ [val node] ++ (bstToVec $ right node)
@@ -171,9 +197,11 @@ bstToVec node = (bstToVec $ left node) ++ [val node] ++ (bstToVec $ right node)
 vecToBst :: Ord a => [a] -> BST a
 vecToBst = foldl(\acc x -> bstinsert x acc) Void
 
+--4.7
 treeord :: Ord a => [a] -> [a]
 treeord = bstToVec . vecToBst
 
+--4.8
 filtertree :: (a -> Bool) -> BST a -> [a]
 filtertree p = (filter p) . bstToVec
 
@@ -181,6 +209,7 @@ nodeh :: BST (a, Integer) -> Integer
 nodeh Void = 0
 nodeh x = snd $ val x
 
+--4.9
 annotate :: (Ord a) => BST a -> BST (a, Integer)
 annotate Void = Void
 annotate node = Node { val = (val node, h+1), left=l, right=r}
@@ -189,6 +218,7 @@ annotate node = Node { val = (val node, h+1), left=l, right=r}
     r = annotate $ right node
     h = max (nodeh l) (nodeh r)
 
+--4.10
 almostBalanced :: Ord a => BST a -> Bool
 almostBalanced x = aux $ annotate x
   where
@@ -201,6 +231,7 @@ almostBalanced x = aux $ annotate x
 
 data WBST a = WVoid | WNode a Int ( WBST a) ( WBST a) deriving (Read, Show)
 
+--4.11
 wbstinsert x WVoid = WNode x 1 WVoid WVoid
 wbstinsert x (WNode val h left right) = WNode val h l r
   where
@@ -213,6 +244,7 @@ wbstinsert x (WNode val h left right) = WNode val h l r
 a = foldl(\acc x -> bstinsert x acc) Void [2,3,0,8,6]
 b = foldl(\acc x -> wbstinsert x acc) WVoid [2,3,0,5,4]
 
+--4.12
 diff2next tree = fst $ aux tree diffs
   where
     visitOrd = bstToVec tree
@@ -224,6 +256,7 @@ diff2next tree = fst $ aux tree diffs
         newNode = Node {val = (val node, head newDiffsL), left = newNodeL, right = newNodeR}
         (newNodeR, newDiffs) = aux (right node) (tail newDiffsL)
 
+--4.13
 bstlevels :: Ord a => BST a -> [a]
 bstlevels tree = map snd $ treeord $ map (\(a,b) -> (-b,a)) $ bstToVec $ annotate tree
 
@@ -231,10 +264,13 @@ fold :: (Ord a) => (a -> b -> b -> b) -> b -> BST a -> b
 fold _ z Void = z
 fold f z ( Node x l r) = f x ( fold f z l) ( fold f z r)
 
+--4.14
 treeheight' tree = fold (\x l r -> 1 + (max l r)) 0 tree
 
+--4.15
 annotate' tree = fold (\x l r -> Node {val=(x, 1+(max (nodeh l) (nodeh r))), left=l, right=r}) Void $ tree
 
+--4.16
 almostBalanced' :: Ord a => BST a -> Bool
 almostBalanced' Void = True
 almostBalanced' tree = fst $ val $  fold aux Void $ tree
@@ -251,9 +287,11 @@ diameter tree = fst $ fold (\x l r -> (maxh x l r, h x l r)) (0, -1) tree
     maxh x l r = maximum [fst l, fst r, 2 + (snd l) + (snd r)]
     h x l r = 1 + (max (snd l) (snd r))
 
+--4.17
 maxDiameter :: Ord a => [BST a] -> Integer
 maxDiameter = maximum . map diameter
 
+--4.18
 isBst :: Ord a => BST a -> Bool
 isBst tree = ok' $ fold (\x l r -> ((isok x l r, True), (newmin x l r, newmax x l r))) ((True, False), (val tree, val tree)) tree
   where
@@ -275,14 +313,22 @@ data Bal = Left | Bal | Right deriving (Eq , Ord , Read , Show )
 
 -- todo alberi ricerca 19, 20
 
+--4.21
 bst2List' :: Ord a => BST a -> [a]
 bst2List' tree = fold (\x l r -> l ++ [x] ++ r) [] tree
 
-filtertree :: (a -> Bool) -> BST a -> [a]
-filtertree' p = fold (\x l r -> l ++ (if p x then [x] else []) ++ r) []
+--filtertree' :: (a -> Bool) -> BST a -> [a]
+--filtertree' p = fold (\x l r -> l ++ (if p x then [x] else []) ++ r) []
 
 
 
+-- Alberi generici
+
+data (Eq a, Show a) => Tree a = Void | Node a [Tree a]
+  deriving (Eq, Show)
+
+treefold :: (Eq a, Show a) => (a -> [b] -> b) -> b -> Tree a -> b
+treefold =
 
 --a = [[1,2,3], [1, 1, 1]]
 --b = [[5,1], [4, 1], [1, 2]]
